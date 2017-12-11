47554dc0010b84437ffadbb165968f6cceb19286
devcode-Su <dev.code.su@gmail.com>
2017-12-11 19:07:24 +0900

    datataable 수정

--- src/components/dashboard/thumb/Thumb.datatable.select.vue
+++ src/components/dashboard/thumb/Thumb.datatable.select.vue
@@ -103,4 +103,7 @@ export default {
 </script>
 <style lang='scss' scoped>
 @import "~styles/variables";
+.col-ip{
+  width: 115px;
+}
 </style>
--- src/components/dashboard/thumb/Thumb.datatable.vue
+++ src/components/dashboard/thumb/Thumb.datatable.vue
@@ -96,4 +96,7 @@ export default {
 </script>
 <style lang='scss' scoped>
 @import "~styles/variables";
+  .col-ip{
+    width: 115px;
+  }
 </style>
