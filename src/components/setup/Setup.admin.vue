b7fc41ddcb6617fa1dd84b397c81d3054dfae105
devcode-Su <dev.code.su@gmail.com>
2017-12-11 19:09:35 +0900

    관리자 설정 적용중 - 삭제 / 수정 남음

--- src/components/setup/Setup.admin.vue
+++ src/components/setup/Setup.admin.vue
@@ -3,20 +3,39 @@
     <h1 data-page-title>
       관리자 설정
     </h1>
-    <div>
-      comming soon
+    <div data-table="table">
+      <span data-table="total">전체 : {{adminList.length}}명</span>
+      <div data-thead="thead">
+        <table>
+          <thead>
+          <tr>
+            <th v-for="(th,k) in dataFields" :key="k" :class="'col-'+k">{{th}}</th>
+          </tr>
+          </thead>
+        </table>
+      </div>
+      <div data-tbody="tbody">
+        <table>
+          <tbody>
+          <tr data-tbody="row" v-for="row in adminList" :key="row.id" @dblclick="modify(row)">
+            <td v-for="(td,k) in dataFields" :key="k" :class="'col-'+k">{{row[k]}}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <div data-tooltip-wrap="setup">
-      <button data-icon type="text" @click="showModal = true">
+      <button data-icon type="text" @click="setAdmin">
         <i class="fa fa-user-plus fa-lg" aria-hidden="true"></i>
         <span data-tooltip="setup">관리자 추가</span>
       </button>
     </div>
-
-    <templatemodal v-if="showModal" @close="showModal = false" :target="'user-custom'" :compSelect="'systemadminadd'" :title="'관리자 추가 등록'"></templatemodal>
+    <templatemodal v-if="modal" :target="false" :compSelect="'systemadminadd'" :title="title"></templatemodal>
   </article>
 </template>
 <script>
+  import Constant from "@/constant";
+  import { mapGetters } from "vuex";
 import Templatemodal from "../template/Template.modal";
 
 export default {
@@ -27,24 +46,68 @@ export default {
   },
   data() {
     return {
-      showModal: false
+      showModal: false,
+      receiveData : [],
+      title : "",
+      //adminList : [],
+      dataFields : {
+        id : "아이디",
+        name : "이름",
+        host : "Host",
+        description : "설명",
+        last_login_time : "마지막 접속시각"
+      }
     };
   },
-  computed: {},
+  computed: {
+    ...mapGetters({
+      adminList : "setupAdmin",
+      modal : "setupModal"
+    })
+  },
   components: {
     "templatemodal":Templatemodal
   },
-  watch: {},
-  methods: {},
+  watch: {
+    adminList(a){
+      if(a){
+        return a;
+      }
+    }
+  },
+  methods: {
+    getList(){
+      this.$store.dispatch(Constant.SETUP_ADMIN);
+    },
+    setAdmin(){
+      this.title = "관리자 등록";
+      this.$store.commit(Constant.SETUP_MODAL, true);
+    },
+    modify(row){
+      this.title = "관리자 수정";
+      this.$store.commit(Constant.SETUP_MODAL, true);
+      this.$store.commit(Constant.SETUP_FORMTYPE, false);
+      this.$store.commit(Constant.PICK_ADMIN, row.id);
+    }
+  },
   beforeCreate() {},
-  created() {},
+  created() {
+    this.getList();
+    this.$bus.$on("admin", this.getList);
+    this.$bus.$on("modalClose", data => {
+      this.showModal = data;
+    });
+  },
   beforeMounted() {},
   mounted() {},
   beforeUpdate() {},
   updated() {},
   actvated() {},
   deactivated() {},
-  beforeDestroy() {},
+  beforeDestroy() {
+    this.$bus.$off("admin");
+    this.$bus.$off("modalClose");
+  },
   destroyed() {}
 };
 </script>
@@ -54,4 +117,24 @@ export default {
   width: 24px;
   height: 24px;
 }
+  [data-table="table"]{
+    margin-top:40px;
+  }
+  [data-tbody="tbody"]{
+    height:100vh;
+    max-height:700px;
+    border-bottom:1px solid color(border)
+  }
+  .col-id{
+    width:100px;
+  }
+  .col-name{
+    width:150px;
+  }
+  .col-host{
+    width:200px;
+  }
+  .col-last_login_time{
+    width:170px;
+  }
 </style>
---
+++ src/components/setup/Setup.adminForm.vue
@@ -0,0 +1,207 @@
+<template>
+  <section>
+    <div>
+      <form @submit.prevent="submitForm(form)">
+        <fieldset>
+          <div data-form-item>
+            <label data-form-label="required">아이디</label>
+            <div data-form-tag>
+              <el-input id="id" type="text" v-model="form.id" size="small" :class="{ 'is-error' : required.id}" clearable @blur="requiredCheck"></el-input>
+              <span data-required-msg v-if="required.id">빈칸을 채워주세요.</span>
+            </div>
+          </div>
+          <div data-form-item>
+            <label data-form-label="required">이름</label>
+            <div data-form-tag>
+              <el-input id="name" type="text" v-model="form.name" size="small" :class="{ 'is-error' : required.name}" clearable @blur="requiredCheck"></el-input>
+              <span data-required-msg v-if="required.name">이름을 입력하세요.</span>
+            </div>
+          </div>
+          <div data-form-item>
+            <label data-form-label="required">Host</label>
+            <div data-form-tag>
+              <el-input id="host" type="text" v-model="form.host" size="small" :class="{ 'is-error' : required.host}" clearable @blur="requiredCheck"></el-input>
+              <span data-required-msg v-if="required.host">Host IP 를 입력하세요.</span>
+            </div>
+          </div>
+          <div data-form-item>
+            <label data-form-label="required">이메일</label>
+            <div data-form-tag>
+              <el-input id="email" type="email" v-model="form.email" size="small" :class="{ 'is-error' : required.email}" clearable @blur="requiredCheck" required></el-input>
+              <span data-required-msg v-if="required.email">이메일을 입력하세요.</span>
+            </div>
+          </div>
+          <div data-form-item>
+            <label data-form-label="required">비밀번호</label>
+            <div data-form-tag>
+              <el-input id="passwd" type="password" v-model="form.passwd" size="small" :class="{ 'is-error' : required.passwd}" clearable @blur="requiredCheck"></el-input>
+              <span data-required-msg v-if="required.passwd">빈칸을 채워주세요.</span>
+            </div>
+          </div>
+          <div data-form-item>
+            <label>설명</label>
+            <div data-form-tag>
+              <el-input id="description" type="text" v-model="form.description" size="small" clearable></el-input>
+            </div>
+          </div>
+          <div data-button v-if="formBtnType" >
+            <el-button size="small" native-type="submit">저장</el-button>
+            <el-button type="info" size="small" plain @click="setCancel">취소</el-button>
+          </div>
+          <div data-button v-else>
+            <el-button size="small" native-type="submit">수정</el-button>
+            <el-button type="danger" size="small" plain @click="remove">삭제</el-button>
+            <el-button type="info" size="small" plain @click="setCancel">취소</el-button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  </section>
+</template>
+<script>
+  import Constant from "@/constant";
+  import { mapGetters } from "vuex";
+export default {
+  name: "Systemadminadd",
+  extends: {},
+  props: {
+    //알파벳 순으로 정렬할 것.
+  },
+  data() {
+    return {
+      required: {
+        id: false,
+        name : false,
+        host : false,
+        email : false,
+        passwd : false
+      },
+      form: {
+        id: "",
+        name:"",
+        host: "",
+        email: "",
+        passwd: "",
+        description: ""
+      },
+    };
+  },
+  computed: {
+    ...mapGetters({
+      insertForm :"pickAdmin",
+      formBtnType : "setupFormType"
+    })
+  },
+  components: {},
+  watch: {
+  },
+  methods: {
+    requiredCheck(val){
+      console.log(val);
+      if(val.target.id === "id"){
+        if(val.target.value === "") this.required.id = true;
+        else this.required.id = false
+      }else if(val.target.id === "name"){
+        if(val.target.value === "") this.required.name = true;
+        else this.required.name = false
+      }else if(val.target.id === "host"){
+        if(val.target.value === "") this.required.host = true;
+        else this.required.host = false
+      }else if(val.target.id === "email"){
+        if(val.target.value === "") this.required.email = true;
+        else this.required.email = false
+      }else if(val.target.id === "passwd"){
+        if(val.target.value === "") this.required.passwd = true;
+        else this.required.passwd = false
+      }
+    },
+    submitForm(form) {
+      console.log(form);
+      if(form.id === "" || form.name === "" || form.host === "" || form.email === "" || form.passwd === ""){
+        if(form.id === "") this.required.id = true;
+        if(form.name === "") this.required.name = true;
+        if(form.host === "") this.required.host = true;
+        if(form.email === "") this.required.email = true;
+        if(form.passwd === "") this.required.passwd = true;
+      }else {
+        if (this.formBtnType) {
+          console.log("등록");
+          //const url = "/api/admin";
+          // this.$http.post(url, this.form);
+          // this.$bus.$emit("admin");
+          // this.$bus.$emit("modalClose", false);
+        } else {
+          console.log("수정")
+        }
+      }
+    },
+    setCancel(){
+      // this.form.id = "";
+      // this.form.name = "";
+      // this.form.host = "";
+      // this.form.email = "";
+      // this.form.passwd = "";
+      // this.form.description = "";
+      this.$store.commit(Constant.SETUP_MODAL, false);
+      this.$store.commit(Constant.SETUP_FORMTYPE, true);
+      this.$store.commit(Constant.PICK_ADMIN, null);
+    },
+    remove(){
+      this.$confirm('관리자를 삭제합니까?', '주의', {
+        confirmButtonText: '삭제',
+        cancelButtonText: '취소',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '삭제가 완료됐습니다.'
+        });
+        //let id = this.form.id;
+        this.$store.dispatch(Constant.DELETE_ADMIN, this.form.id);
+        this.setCancel();
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '삭제가 취소됐습니다.'
+        });
+        this.setCancel();
+      });
+    }
+  },
+  beforeCreate() {},
+  created() {
+    if(this.insertForm.length){
+      const form = this.insertForm[0];
+      this.form.id = form.id;
+      this.form.name = form.name;
+      this.form.host = form.host;
+      this.form.email = form.email;
+      this.form.passwd = form.passwd;
+      this.form.description = form.description;
+    }
+  },
+  beforeMounted() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  actvated() {},
+  deactivated() {},
+  beforeDestroy() {
+    this.$bus.$off("modify")
+  },
+  destroyed() {}
+};
+</script>
+<style lang='scss' scoped>
+@import "~styles/variables";
+section{
+  padding:30px;
+}
+.el-form {
+  width: 100%;
+}
+ [data-button]{
+    display:flex;
+    justify-content: flex-end;
+  }
+</style>
