706bd83061165eef684c29a438206bda9ddb41f0
devcode-Su <dev.code.su@gmail.com>
2017-12-19 10:43:21 +0900

    대시보드

--- src/components/dashboard/Dashboard.period.btn.vue
+++ src/components/dashboard/Dashboard.period.btn.vue
@@ -1,38 +1,48 @@
 <template>
   <div class="dashboard-period-btn">
-    <el-button v-for="(sort, i) in categorize" :key="sort.id" size="small" @click="btnClick(i)">{{sort}}</el-button>
+    <el-button v-for="(sort, i) in categorize" :key="sort.id" size="small" @click="btnClick(i)" :class="{selected : i === idx}">{{sort}}</el-button>
   </div>
 </template>
 <script>
-export default {
-  name: "DashboardPeriodbtn",
-  extends: {},
-  props: {
-    categorize: {
-      type: Array
-    }
-  },
-  data() {
-    return {};
-  },
-  components: {
-    idx: 0
-  },
-  methods: {
-    btnClick(index) {
-      if (this.idx !== index) {
-        this.$emit("periodClick", index);
-        this.idx = index;
+  export default {
+    name: "DashboardPeriodbtn",
+    extends: {},
+    props: {
+      categorize: {
+        type: Array
       }
-    }
-  },
-  mounted() {}
-};
+    },
+    data() {
+      return {
+        idx: 0
+      };
+    },
+    components: {
+
+    },
+    methods: {
+      btnClick(index) {
+        if (this.idx !== index) {
+          this.$emit("periodClick", index);
+          this.idx = index;
+        }
+      }
+    },
+    mounted() {}
+  };
 </script>
 <style lang='scss' scoped>
-@import "~styles/variables";
-.el-button {
-  height: 24px;
-  padding: 0 10px;
-}
+  @import "~styles/variables";
+  .el-button {
+    height: 24px;
+    padding: 0 10px;
+    &.selected{
+      color:#409eff;
+      background-color:#ecf5ff;
+      border-color:#c6e2ff;
+      span{
+        color:#409eff;
+      }
+    }
+  }
 </style>
