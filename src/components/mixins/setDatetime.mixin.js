0551a8cfcf78796c9ef844be06f7da3bab10260d
devcode-Su <dev.code.su@gmail.com>
2017-12-19 10:44:08 +0900

    믹스인 date

--- src/components/mixins/setDatetime.mixin.js
+++ src/components/mixins/setDatetime.mixin.js
@@ -9,29 +9,62 @@ export default {
           this.startDate = new Date();
           this.startDate.setHours(0);
           this.startDate.setMinutes(0);
-          this.startDate.setSeconds(0);
-          this.startDate.setMilliseconds(0);
+          //this.startDate.setSeconds(0);
+          //this.startDate.setMilliseconds(0);
           break;
         case 2:
           this.startDate = new Date();
           this.startDate.setDate(this.startDate.getDate() - ((this.startDate.getDay() + 7 - 1) % 7));
           this.startDate.setHours(0);
           this.startDate.setMinutes(0);
-          this.startDate.setSeconds(0);
-          this.startDate.setMilliseconds(0);
+          //this.startDate.setSeconds(0);
+          //this.startDate.setMilliseconds(0);
           break;
         case 3:
           this.startDate = new Date();
           this.startDate.setDate(1);
           this.startDate.setHours(0);
           this.startDate.setMinutes(0);
-          this.startDate.setSeconds(0);
-          this.startDate.setMilliseconds(0);
+          //this.startDate.setSeconds(0);
+          //this.startDate.setMilliseconds(0);
           break;
         default:
           break;
       }
       this.endDate = new Date();
+    },
+    setFormDateTime(num) {
+      switch (num) {
+        case 0:
+          this.form.startDate = new Date(new Date().getTime() - 3600 * 1000 * 1);
+          break;
+        case 1:
+          this.form.startDate = new Date();
+          this.form.startDate.setHours(0);
+          this.form.startDate.setMinutes(0);
+          //this.form.startDate.setSeconds(0);
+          //this.form.startDate.setMilliseconds(0);
+          break;
+        case 2:
+          this.form.startDate = new Date();
+          this.form.startDate.setDate(this.form.startDate.getDate() - ((this.form.startDate.getDay() + 7 - 1) % 7));
+          this.form.startDate.setHours(0);
+          this.form.startDate.setMinutes(0);
+          //this.form.startDate.setSeconds(0);
+          //this.form.startDate.setMilliseconds(0);
+          break;
+        case 3:
+          this.form.startDate = new Date();
+          this.form.startDate.setDate(1);
+          this.form.startDate.setHours(0);
+          this.form.startDate.setMinutes(0);
+          //this.form.startDate.setSeconds(0);
+          //this.form.startDate.setMilliseconds(0);
+          break;
+        default:
+          break;
+      }
+      this.form.endDate = new Date();
     },
     setDatelast(num) {
       switch (num) {
